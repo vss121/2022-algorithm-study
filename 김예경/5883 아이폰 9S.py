4750c9c07eafca9d06fbf822c52d417912744bf9
vss121 <yk_kim@chungbuk.ac.kr>
2022-07-17 22:57:38 +0900

    [3주차] : 김예경 - 5883 아이폰 9S Silver4 (CBNU-Nnet#60)

---
+++ 김예경/5883 아이폰 9S.py
@@ -0,0 +1,23 @@
+n=int(input())
+a=list()
+for _ in range(n):
+    a.append(int(input()))
+a_set = set(a)
+
+res=0
+for x in a_set:
+    cnt=1
+    prev=a[0]
+    for i in range(1,n):
+        if x==a[i]:
+            continue
+        if prev==a[i]:
+            cnt+=1
+        else:
+            prev=a[i]
+            cnt=1
+        
+        if cnt>res:
+            res=cnt
+
+print(res)
